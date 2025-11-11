f5901dc129a8dae6c9a648ecf8d15fd56a55facd
MrSombra203 <jose.vasconez.andrade@udla.edu.ec>
2025-11-11 11:25:23 -0500

    Agregar archivos de proyecto.

---
+++ Proyecto_Analisis_de_crrimen/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Proyecto_Analisis_de_crrimen.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
